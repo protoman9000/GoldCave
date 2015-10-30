38a14b7944c770018cf91b3e8b1167aa725acd50
protoman9000 <oyekan900@yahoo.com>
2015-10-30 00:06:48 -0700

    Intro and asking the user for the backpack weight.

--- GoldCave/Program.cs
+++ GoldCave/Program.cs
@@ -4,12 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace GoldCave
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("You are about to enter the golden cave...");
+            Console.WriteLine("You are wearing a backpack. How much does your backpack weight?");
+            string backpack = Console.ReadLine();
+            int gBackpack = Convert.ToInt16(backpack);
+            decimal gBackpack2 = decimal.Round(7, gBackpack);
+
         }
     }
 }
