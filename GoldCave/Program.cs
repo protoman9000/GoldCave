8664e51678a5fc4d38172e6183ba82147231194c
protoman9000 <oyekan900@yahoo.com>
2015-11-02 22:37:27 -0800

    Creating a list for the gold nuggets

--- GoldCave/Program.cs
+++ GoldCave/Program.cs
@@ -17,6 +17,21 @@ namespace GoldCave
             int gBackpack = Convert.ToInt16(backpack);
             decimal gBackpack2 = decimal.Round(7, gBackpack);
 
+            Console.WriteLine("How many gold nuggets do you see?");
+            string goldPieces = Console.ReadLine();
+            int gPieces = Convert.ToInt16(goldPieces);
+
+            List<double> goldList = new List<double>();
+
+            for (int x = 1; x <= gPieces; x++)
+            {
+                int section = 0;
+                Console.WriteLine("Enter the in gold piece number {0}", x);
+                string piece = Console.ReadLine();
+                goldList[section] = Convert.ToDouble(piece);
+                section++;
+            }
+
         }
     }
 }
